302b581507cb873fb9dfbaf5dce36f30f5e00472
waterbom <159856504+waterbom@users.noreply.github.com>
2024-02-15 10:00:06 +0900

    ods.v

--- 02_javascript/240215/00_js_if/js/index.js
+++ 02_javascript/240215/00_js_if/js/index.js
@@ -1,28 +1,13 @@
-// let kor_score = Number(prompt('국어 점수'));
-// let eng_score = Number(prompt('영어 점수'));
-// let math_score = Number(prompt('수학 점수'));
+// let number = Number(prompt('숫자를 입력하세요'));
 
-let kor_score = 70;
-let eng_score = 80;
-let math_score = 90;
+// if (number >= 50) {
+//     console.log('크다');
+//     document.write('크다');
+// }   else {
+//     console.log('작다');
+//     document.write('작다');
+// }
 
-let avg = (kor_score + eng_score + math_score) / 3;
+let msg = (number >= 50) ? "크다" : "작다";
+console.log(msg);
 
-// 100 수 90 우 80 미 70 양 60 가
-let result;
-
-if (avg >= 90) result = '수';
-else if (avg >= 80) result = '우';
-else if (avg >= 70) result = '미';
-else if (avg >= 60) result = '양';
-else result = '가';
-
-let msg = `<h1>당신의 성적은? ${result}</h1>`
-document.write(msg);
-
-let readHbrb = true;
-let readKjpj = true;
-
-if (readHbrb || readKjpj) {
-    document.write('하나라도 읽어서 다행');
-}
