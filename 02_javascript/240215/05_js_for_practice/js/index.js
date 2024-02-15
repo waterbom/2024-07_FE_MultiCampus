81c4253ea47898caa875e897aa26777e01ec0c4e
InkyuInst <484342.plus@gmail.com>
2024-02-15 11:51:44 +0900

    for loop

--- 02_javascript/240215/05_js_for_practice/js/index.js
+++ 02_javascript/240215/05_js_for_practice/js/index.js
@@ -33,3 +33,18 @@ for (let i = 1; i <= num; i++) {
     // i = 4, 4 <= 3 ---> 거짓 => 조건문 종료
     console.log(`Hello Javascript ${i}번째 출력`);
 }
+
+/* 
+// 모범 답안
+// 사용자 입력 받기
+let numberOfTimes = parseInt(prompt("반복 횟수를 입력하세요:")); // 입력 문자열을 정수로 변환
+// 입력값이 유효한 숫자인지 확인
+if (!isNaN(numberOfTimes)) {
+    // 사용자가 입력한 숫자만큼 반복
+    for (let i = 0; i < numberOfTimes; i++) {
+        console.log("Hello JavaScript");
+    }
+} else {
+    console.log("유효한 숫자를 입력해 주세요.");
+}
+*/
