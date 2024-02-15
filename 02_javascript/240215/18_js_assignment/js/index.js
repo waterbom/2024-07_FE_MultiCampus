a9e70110d4362a5e3550d0e8c5238cd839564443
InkyuInst <484342.plus@gmail.com>
2024-02-15 17:03:28 +0900

    240215 condition, loop, function

--- 02_javascript/240215/18_js_assignment/js/index.js
+++ 02_javascript/240215/18_js_assignment/js/index.js
@@ -1,4 +1,4 @@
-let answer = Math.floor(Math.random() * 32);
+let answer = Math.floor(Math.random() * 31 + 1);
 // 1. 사용자로부터 정답이 입력될 때까지 숫자를 입력받는다. (hint. while)
 // 2. 입력은 0보다 큰 값만 입력 가능하다. (0보다 작다면 다시 입력 _ hint. continue)
 // 3. 정답보다 작으면 down을, 정답보다 크면 up을 출력하고, 계속 숫자를 입력받는다.
