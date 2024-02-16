540df644c9596eca51722037c8620cbfcf92f3d0
waterbom <159856504+waterbom@users.noreply.github.com>
2024-02-16 10:00:57 +0900

    20240215

--- 02_javascript/240215/18_js_assignment/js/index.js
+++ 02_javascript/240215/18_js_assignment/js/index.js
@@ -1,4 +1,22 @@
-let answer = Math.floor(Math.random() * 31 + 1);
+let answer =  Math.floor(Math.random() * 31 + 1);
 // 1. 사용자로부터 정답이 입력될 때까지 숫자를 입력받는다. (hint. while)
 // 2. 입력은 0보다 큰 값만 입력 가능하다. (0보다 작다면 다시 입력 _ hint. continue)
 // 3. 정답보다 작으면 up을, 정답보다 크면 down을 출력하고, 계속 숫자를 입력받는다.
+
+let i = 1;
+
+while (1) {
+    let num = Number(prompt('숫자를 입력하세요'));
+    if (num <= 0) {
+        alert('0보다 큰 숫자를 입력해주세요');
+        continue;
+    } else if (num == answer) {
+        document.write(`정답입니다 ! (${answer}) ${i}번만에 성공하셨습니다 !`);
+        break;
+    } i++;
+    if (num < answer) {
+        alert('Up ↑');
+    } else if (num > answer) {
+        alert('Down ↓');
+    }
+}
