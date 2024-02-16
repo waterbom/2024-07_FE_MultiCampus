efc547fd5e98ea2c2f6941bf9a97c745aa26041f
InkyuInst <484342.plus@gmail.com>
2024-02-16 11:19:00 +0900

    arrow, callback

---
+++ 02_javascript/240216/03_js_set_interval/js/index.js
@@ -0,0 +1,28 @@
+// setInterval(콜백함수, 시간)
+
+// function greeting() {
+//     document.write('안녕하세요<br/>');
+// }
+// setInterval(greeting, 2000);
+
+// let cnt = 0;
+// let timer = setInterval(() => {
+//     document.write('안녕하세요<br/>')
+//     cnt++;
+//     if (cnt == 2) {
+//         document.write('안녕히 계세요<br/>');
+//         clearInterval(timer);
+//     }
+// }, 2000);
+
+let cnt = 0;
+function timer() {
+    document.write('안녕하세요<br/>')
+    cnt++;
+    if (cnt == 2) {
+        document.write('안녕히 계세요<br/>');
+        clearInterval(timer);
+    }
+}
+
+setInterval(timer, 2000);
