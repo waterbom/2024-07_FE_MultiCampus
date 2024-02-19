9bba18a28fe6ac9303659ae98b6e9e1b03a08e08
Inkyu24 <484342@gmail.com>
2024-02-19 17:32:34 +0900

    240219

--- 02_javascript/240219/09_js_event_listener_practice01/js/index.js
+++ 02_javascript/240219/09_js_event_listener_practice01/js/index.js
@@ -1,10 +1,14 @@
-// 마우스 오버하면 이미지가 바뀌도록
-let img = document.querySelector('img');
-let index = 2;
-img.addEventListener("mouseover", () => {
-    img.src = `images/pic-${index % 6 + 1}.jpg`;
-    index++;
-});
+// // 마우스 오버하면 이미지가 바뀌도록
+// let img = document.querySelector('img');
+// let index = 1;
+// img.addEventListener("mouseover", () => {
+//     img.src = `images/pic-${index % 6 + 1}.jpg`;
+//     index++;
+// });
+// img.addEventListener("mouseout", () => {
+//     img.src = `images/pic-${index % 6 + 1}.jpg`;
+//     index++;
+// });
 
 /* 
 click	사용자가 동일한 요소 위에서 마우스 버튼을 눌렀다 땔 때
@@ -15,3 +19,13 @@ mousemove	마우스를 움직였을 때
 mouseover	요소 위로 마우스를 움직였을 때
 mouseout	요소 바깥으로 마우스를 움직였을 때
 */
+
+const imgBox = document.querySelector("#container > img");
+
+imgBox.addEventListener("mouseover", () => imgBox.src = "images/pic-2.jpg");
+imgBox.addEventListener("mouseout", () => imgBox.src = "images/pic-1.jpg");
+imgBox.addEventListener("click", () => imgBox.src = "images/pic-3.jpg");
+imgBox.addEventListener("dblclick", () => imgBox.src = "images/pic-3.jpg");
+imgBox.addEventListener("mousedown", () => imgBox.src = "images/pic-4.jpg");
+imgBox.addEventListener("mousemove", () => imgBox.src = "images/pic-5.jpg");
+imgBox.addEventListener("mouseup", () => imgBox.src = "images/pic-6.jpg");
