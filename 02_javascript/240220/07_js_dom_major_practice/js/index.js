af000c0310900d371d59ac327cbc51cb4dd96f8e
Inkyu24 <484342@gmail.com>
2024-02-20 14:31:29 +0900

    major complete

--- 02_javascript/240220/07_js_dom_major_practice/js/index.js
+++ 02_javascript/240220/07_js_dom_major_practice/js/index.js
@@ -1,1 +1,39 @@
-// 
+// 1. 등록버튼을 눌렀을 때, 이름과 전공을 콘솔에 출력 (성공)
+const addBtn = document.querySelector('button');
+
+let add = () => {    
+    let name = document.querySelector('#username').value; // 홍길동
+    let major = document.querySelector('#major').value; // 컴퓨터
+    // console.log(`${name}, ${major}`);
+
+    if (name && major) {
+        // 2. <tr> <td>${name}</td><td>${major}</td> </tr>
+        let tr = document.createElement('tr'); // <tr></tr>
+
+        let tdName = document.createElement('td'); // <td></td>
+        let nameText = document.createTextNode(name); // 텍스트 노드 : 홍길동
+        tdName.appendChild(nameText); // tdName = <td>홍길동</td>
+
+        let tdMajor = document.createElement('td'); // <td></td>
+        tdMajor.innerText = major; // tdMajor = <td>컴퓨터</td>
+
+        tr.appendChild(tdName); // <tr><td>홍길동</td></tr>
+        tr.appendChild(tdMajor); // <tr><td>홍길동</td><td>컴퓨터</td></tr>
+
+        document.querySelector('#attendant > tbody').appendChild(tr);
+        document.querySelector('#username').value = '';
+        document.querySelector('#major').value = '';
+        document.querySelector('#username').focus();
+    }
+}
+
+addBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    add();
+});
+
+document.querySelector('#major').addEventListener('keyup', (e) => {
+    if (e.code == "Enter") {
+        add();
+    }
+})
