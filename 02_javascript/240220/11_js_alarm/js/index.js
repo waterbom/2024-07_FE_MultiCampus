dd55c5f0cff7216581cbcc85eb88421d2482698c
Inkyu24 <484342@gmail.com>
2024-02-20 17:09:43 +0900

    bar

--- 02_javascript/240220/11_js_alarm/js/index.js
+++ 02_javascript/240220/11_js_alarm/js/index.js
@@ -12,17 +12,30 @@ btn.addEventListener('click', ()=> {
     // 5) 클래스 이름(.noti) 추가하기
     noti.classList.add('noti');
     // 6) 텍스트 넣기
-    noti.innerText = '알림 3초 후에 사라집니다.'
-
-    /* 서비스 */
-    let bar = document.createElement('div');
-    bar.classList.add('bar');
-    noti.appendChild(bar);
+    let second = 3;
+    noti.innerText = `알림 ${second}초 후에 사라집니다.`;
 
     // 7) noti-box 안에 div.noti 넣기
     notiBox.appendChild(noti);
+
+    /* 진행바 */
+    let bar = document.createElement('div');
+    bar.classList.add('bar');
+    notiBox.appendChild(bar)
+
     // 8) 3초 후에 사라지게 하기
-    setTimeout(()=> noti.remove(), 3000);
+    setTimeout(()=> {
+        noti.remove();
+        bar.remove();
+    }, 3000);
+
+    let timer = setInterval(()=>{
+        second--;
+        noti.innerText = `알림 ${second}초 후에 사라집니다.`;
+        if(second <= 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
 })
 
 
