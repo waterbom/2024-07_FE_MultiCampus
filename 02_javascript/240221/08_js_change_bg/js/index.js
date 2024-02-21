3cb696e2d46911519444af81f7b0fe79ef4bc309
Inkyu24 <484342@gmail.com>
2024-02-21 17:44:56 +0900

    duplicate

--- 02_javascript/240221/08_js_change_bg/js/index.js
+++ 02_javascript/240221/08_js_change_bg/js/index.js
@@ -6,5 +6,13 @@ window.addEventListener('load', () => {
     
     const imgArr = ['bg-1.jpg', 'bg-2.jpg', 'bg-3.jpg', 'bg-4.jpg', 'bg-5.jpg'];
     let randomNum = Math.floor(Math.random() * imgArr.length);
+    let x_img = sessionStorage.getItem('bg');
+    
+    console.log(x_img, imgArr[randomNum]);
+    while (x_img == imgArr[randomNum] && imgArr.length != 1) {
+        randomNum = Math.floor(Math.random() * imgArr.length);
+    }
+    
+    sessionStorage.setItem('bg', imgArr[randomNum]);
     document.body.style.backgroundImage = `url(./images/${imgArr[randomNum]})`
 })
