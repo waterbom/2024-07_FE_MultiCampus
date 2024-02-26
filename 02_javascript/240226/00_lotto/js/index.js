72ffdb88384df5ea2b717b5b6032c0fc8b96adc8
Inkyu24 <484342@gmail.com>
2024-02-26 09:46:30 +0900

    lotto

--- 02_javascript/240226/00_lotto/js/index.js
+++ 02_javascript/240226/00_lotto/js/index.js
@@ -7,3 +7,14 @@ const luckyNumber = {
 };
 
 // 버튼 클릭 시, 로또번호 추출하여 출력
+button.addEventListener('click', () => {
+    const { digitCount, maxNumber } = luckyNumber;
+    // console.log(digitCount, maxNumber);
+    const numberSet = new Set();
+    while (numberSet.size != digitCount) {
+        let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+        // console.log(randomNumber);
+        numberSet.add(randomNumber);
+    }
+    result.innerText = `${[...numberSet].sort((a, b) => a - b)}`; // set > arr [전개연산자 활용]
+});
