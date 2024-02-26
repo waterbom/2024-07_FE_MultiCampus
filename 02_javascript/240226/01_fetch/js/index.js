8b1f8964d40cb6b5aea50a4ae4d5cd76675e88b0
Inkyu24 <484342@gmail.com>
2024-02-26 11:04:19 +0900

    js

--- 02_javascript/240226/01_fetch/js/index.js
+++ 02_javascript/240226/01_fetch/js/index.js
@@ -5,14 +5,30 @@
 // fetch의 post 요청
 // fetch("URL", { method: 'POST', body: ~~~, headers: ~~~ })
 
+const btn = document.querySelector('button');
+let num = 0;
+btn.addEventListener('click', () => {
+    num++;
+    let url = `https://dummyjson.com/products/${num}`;
 
-const url = `https://dummyjson.com/products/1`;
-
-
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const resultDiv = document.querySelector('#result');
-        resultDiv.innerText = data.brand
-    })
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const resultDiv = document.querySelector('#result');
+            resultDiv.innerHTML = `
+                <ul>
+                    <li>${data.title}</li>
+                    <li>${data.brand}</li>
+                    <li>${data.description}</li>
+                </ul>
+            `;
+        })
+        .catch(e => {
+            console.error(e);
+            const resultDiv = document.querySelector('#result');
+            resultDiv.innerHTML = "에러발생";
+        })
+        // .finally(()=> {
+            
+        // })
+})
