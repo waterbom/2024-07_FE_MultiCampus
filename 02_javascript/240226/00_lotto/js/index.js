b8f611e3d4d30322546801d42fdef19217adf474
Inkyu24 <484342@gmail.com>
2024-02-26 09:31:10 +0900

    lotto

---
+++ 02_javascript/240226/00_lotto/js/index.js
@@ -0,0 +1,9 @@
+const result = document.querySelector('#result');
+const button = document.querySelector('button');
+
+const luckyNumber = {
+    digitCount: 6, // 숫자 6개
+    maxNumber: 45, // 최대 숫자 45
+};
+
+// 버튼 클릭 시, 로또번호 추출하여 출력
